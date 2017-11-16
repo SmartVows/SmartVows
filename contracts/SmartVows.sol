c82d63231d33b19ee92443b84ab1606348ac6f51
SmartVows <33518943+SmartVows@users.noreply.github.com>
2017-11-16 13:49:41 -0500

    Update SmartVows.sol

--- contracts/SmartVows.sol
+++ contracts/SmartVows.sol
@@ -7,49 +7,48 @@ import './Util.sol';
 
 contract SmartVows is Ownable, Util {
 
-    // partners for contract
-    string public partner1;
-    string public partner2;
-
-    // Wedding date
-    uint256 public weddingDate;
-
-    //Marriage status
-    bytes32 public marriageStatus;
-
-    // Couple Image
-    bytes public coupleImage;
-
-    // Marriage License Image
-    bytes public marriageLicenceImage;
-
-    // prenup Image
-    bytes public prenupImage;
-
-    // Last Will Image
-    bytes public lastWillImage;
-
+    // Married partner names
+    string public partner1_name;
+    string public partner2_name;
+    
     // Partners eth address
     address public partner1_address;
     address public partner2_address;
-
+    
     // Partners Vows
     string public partner1_vows;
     string public partner2_vows;
 
-    // Partners Signed
+    // Wedding Date
+    uint256 public weddingDate;
+
+    //Marital Status
+    string public maritalStatus;
+
+    // Couple Image Hash
+    bytes public coupleImageIPFShash;
+
+    // Marriage License Image Hash
+    bytes public marriageLicenceImageIPFShash;
+
+    // prenup Text
+    string public prenupAgreement;
+    
+    // Last Will and Testaments
+    string public partner1_will;
+    string public partner2_will;
+
+    // Partners Signed Marriage Contract
     bool public partner1_signed;
     bool public partner2_signed;
+    
+    // Partners Voted to update the prenup
+    bool public partner1_voted_prenup_update;
+    bool public partner2_voted_prenup_update;
 
     // signed the contract
     bool public is_signed;
-
-    // Brides maid
-    string public bridesmaid;
-
-    // Grooms men
-    string public groomsmen;
-
+    
     // Officiant
     string public officiant;
 
@@ -58,7 +57,7 @@ contract SmartVows is Ownable, Util {
 
     // Location of marriage
     string public location;
-
+    
     Event[] public lifeEvents;
 
     struct Event {
@@ -71,23 +70,47 @@ contract SmartVows is Ownable, Util {
     // Declare Life event structure
     event LifeEvent(string name, string description, string url);
 
-    function SmartVows(string _partner1, address _partner1_address, string _partner2, address _partner2_address, uint256 _weddingDate, string url) public{
-        partner1 = _partner1;
-        partner2 = _partner2;
-        partner1_address = _partner1_address;
-        //check if partner 1 address is present
-        if(_partner1_address != address(0)){
-            //transfer ownership
-            transferOwnership(partner1_address);
-        }
-        partner2_address = _partner2_address;
-        weddingDate = _weddingDate;
-        marriageStatus = "married";
-        saveLifeEvent("Marriage", "Marriage registration", url);
+    function SmartVows() public{
+        
+        //For testing
+        partner1_name="Mike";
+        partner2_name="Tiffany";
+        partner1_address=0x1413F25382f9CA2Fd205f8AeaBac3e38b2700136;
+        partner2_address=0x186f931e0853FBdE85b44ffF7B777E33b98D1CA0;
+        weddingDate = 1177113600;
+        maritalStatus = "Married";
+        officiant="Joe Mama";
+        location="Tampa, FL";
+        coupleImageIPFShash="";
+        marriageLicenceImageIPFShash="";
+        
+
+        //Set contract values
+        //partner1_name="%partner1%"
+        //partner2_name ="%partner2%"
+        //partner1_address="%partner1_address%";
+        //partner2_address="%partner2_address%";
+        //weddingDate="%weddingDate%";
+        //maritalStatus="%maritalStatus%";
+        //officiant="%officiant%";
+        //location="%location%";
+        //coupleImageIPFShash="%coupleImageIPFShash%";
+        //marriageLicenceImageIPFShash="%marriageLicenceImageIPFShash%";
+        
+        //Set vows in additional transaction
+        //Update prenup in additional transaction
+        //Update wills in additional transaction
+        
+        //Transfer ownership from SmartVows to Partner 1
+        transferOwnership(partner1_address);
+        
+        //Record contract creation in events
+        saveLifeEvent("Marriage", "Blockchain Marriage contract created", "");
     }
 
-    // Add Life event
-    function addLifeEvent(string name, string description, string url) public onlyOwner{
+    // Add Life event, either partner can update
+    function addLifeEvent(string name, string description, string url) public{
+        require(msg.sender == owner || msg.sender == partner1_address || msg.sender == partner2_address);
         saveLifeEvent(name, description, url);
     }
 
@@ -96,27 +119,14 @@ contract SmartVows is Ownable, Util {
         LifeEvent(name, description, url);
     }
 
-    // Add Partner 1 address only if by owner and already not present
-    function addPartner1Address(address _partner1_address) public onlyOwner{
-        require(partner1_address == address(0));
-        partner1_address = _partner1_address;
-        transferOwnership(partner1_address);
-    }
-
-    // Add Partner 2 address only if by owner and already not present
-    function addPartner2Address(address _partner2_address) public onlyOwner{
-        require(partner2_address == address(0));
-        partner2_address = _partner2_address;
-    }
-
-    // Update Marriage status
-    function updateMarriageStatus(bytes32 _marriageStatus) public {
+    // Update Marriage status, either partner can update
+    function updateMaritalStatus(string _maritalStatus) public {
         require(msg.sender == owner || msg.sender == partner1_address || msg.sender == partner2_address);
-        marriageStatus = _marriageStatus;
+        maritalStatus = _maritalStatus;
         saveLifeEvent("Marriage status updated", strConcat("Marriage status updated by ", toString(msg.sender)), "");
     }
 
-    // Sign the contract
+    // Sign the contract, both partners should sign
     function sign() public {
         require(msg.sender == partner1_address || msg.sender == partner2_address);
         if(msg.sender == partner1_address){
@@ -124,73 +134,61 @@ contract SmartVows is Ownable, Util {
         }else {
             partner2_signed = true;
         }
-        if(partner1_signed && partner2_signed){// if both signed then make the contract as signed
-            is_signed = true;
-        }
         saveLifeEvent("Marriage signed", strConcat("Marriage signed by ", toString(msg.sender)), "");
     }
-
-    // Save coupleImage hash
-    function saveCoupleImage(bytes _coupleImage) public onlyOwner{
-        coupleImage = _coupleImage;
+    
+    //Function to vote to allow for updating prenup, both must vote true
+    function voteToUpdatePrenup() public {
+        if(msg.sender == partner1_address){
+            partner1_voted_prenup_update = true;
+        }
+        if(msg.sender == partner2_address){
+            partner2_voted_prenup_update = true;
+        }
+    }
+    
+    // Update coupleImage hash, either partner can
+    function saveCoupleImage(bytes _coupleImageIPFShash) public{
+        require(msg.sender == owner || msg.sender == partner1_address || msg.sender == partner2_address);
+        coupleImageIPFShash = _coupleImageIPFShash;
     }
 
-    // Save marriage licence image hash
-    function saveMarriageLicenceImage(bytes _marriageLicenceImage) public onlyOwner{
-        marriageLicenceImage = _marriageLicenceImage;
+    // Update marriage licence image hash, either partner can
+    function saveMarriageLicenceImageIPFShash(bytes _marriageLicenceImageIPFShash) public{
+        require(msg.sender == owner || msg.sender == partner1_address || msg.sender == partner2_address);
+        marriageLicenceImageIPFShash = _marriageLicenceImageIPFShash;
     }
 
-    // Save prenup image hash
-    function savePrenupImage(bytes _prenupImage) public onlyOwner{
-        prenupImage = _prenupImage;
+    // Update prenup text, but only if both partners have previously agreed to update the prenup
+    function savePrenupText(string _prenupAgreement) public{
+        require((msg.sender == owner || msg.sender == partner1_address || msg.sender == partner2_address) && (partner1_voted_prenup_update == true)&&(partner2_voted_prenup_update == true));
+        prenupAgreement = _prenupAgreement;
+        partner1_voted_prenup_update = false;
+        partner2_voted_prenup_update = false;
     }
-
-    // Save last will image hash
-    function saveLastWillImage(bytes _lastWillImage) public onlyOwner{
-        lastWillImage = _lastWillImage;
-    }
-
-    // Save Brides maid only once
-    function setBridesmaid(string _bridesmaid) public onlyOwner{
-        require(bytes(bridesmaid).length == 0);
-        bridesmaid = _bridesmaid;
-    }
-
-    // Save grooms men only once
-    function setGroomsmen(string _groomsmen) public onlyOwner{
-        require(bytes(groomsmen).length == 0);
-        groomsmen = _groomsmen;
-    }
-
-    // Save officiant only once
-    function setOfficiant(string _officiant) public onlyOwner{
-        require(bytes(officiant).length == 0);
-        officiant = _officiant;
-    }
-
-    // Save witness only once
-    function setWitness(string _witness) public onlyOwner{
-        require(bytes(witness).length == 0);
-        witness = _witness;
-    }
-
-    // Save Location only once
-    function setLocation(string _location) public onlyOwner{
-        require(bytes(location).length == 0);
-        location = _location;
-    }
-
-    // Save partner 1 vows
-    function setPartner1_vows(string _partner1_vows) public {
-        require(msg.sender == partner1_address);
+    
+    // Update partner 1 vows only once
+    function updatePartner1_vows(string _partner1_vows) public {
+        require((msg.sender == owner || msg.sender == partner1_address) && (bytes(partner1_vows).length == 0));
         partner1_vows = _partner1_vows;
     }
 
-    // Save partner 2 vows
-    function setPartner2_vows(string _partner2_vows) public {
-        require(msg.sender == partner2_address);
+    // Update partner 2 vows only once
+    function updatePartner2_vows(string _partner2_vows) public {
+        require((msg.sender == owner || msg.sender == partner2_address) && (bytes(partner2_vows).length == 0));
         partner2_vows = _partner2_vows;
     }
+    
+    // Update partner 1 will, only partner 1 can update
+    function updatePartner1_will(string _partner1_will) public {
+        require(msg.sender == partner1_address);
+        partner1_will = _partner1_will;
+    }
 
+    // Update partner 2 will, only partner 2 can update
+    function updatePartner2_will(string _partner2_will) public {
+        require(msg.sender == partner2_address);
+        partner2_will = _partner2_will;
+    }
 
 }
